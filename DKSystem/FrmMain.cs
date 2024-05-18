bcc278a707c0b9d6991f13a955491247354c0229
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2024-05-18 12:28:44 +0800

    added system

---
+++ DKSystem/FrmMain.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DKSystem
+{
+    public partial class frmPOS : Form
+    {
+        public frmPOS()
+        {
+            InitializeComponent();
+            Food uc = new Food();
+            addUserControl(uc);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            guna2DataGridView1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void AddCategory()
+        {
+            string qry = "Select * from Category";
+
+
+        }
+        
+        private void sidebarTransition_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnHam_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void sidebar1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void widget1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void widget2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+        private void guna2Button1_Click_1(object sender, EventArgs e)
+        {
+            Food uc = new Food();
+            addUserControl(uc);
+
+        }
+
+        private void guna2Button3_Click_1(object sender, EventArgs e)
+        {
+            Drinks uc = new Drinks();
+            addUserControl(uc);
+        }
+
+        private void guna2Button4_Click(object sender, EventArgs e)
+        {
+            Desserts uc = new Desserts();
+            addUserControl(uc);
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            frmPOS frm = new frmPOS();
+            frm.Show();
+        }
+
+        private void panelContainer_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void menucategory_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint_2(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DKSystem/UseControl/Food.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace DKSystem
+{
+    partial class Food
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Food));
+            this.ucProduct1 = new DKSystem.Model.ucProduct();
+            this.SuspendLayout();
+            // 
+            // ucProduct1
+            // 
+            this.ucProduct1.id = 0;
+            this.ucProduct1.Location = new System.Drawing.Point(3, 3);
+            this.ucProduct1.Name = "ucProduct1";
+            this.ucProduct1.PCategory = null;
+            this.ucProduct1.PImage = ((System.Drawing.Image)(resources.GetObject("ucProduct1.PImage")));
+            this.ucProduct1.PName = "Product Name";
+            this.ucProduct1.PPrice = null;
+            this.ucProduct1.Size = new System.Drawing.Size(256, 254);
+            this.ucProduct1.TabIndex = 0;
+            // 
+            // Food
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.Controls.Add(this.ucProduct1);
+            this.Name = "Food";
+            this.Size = new System.Drawing.Size(501, 489);
+            this.Load += new System.EventHandler(this.Food_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Model.ucProduct ucProduct1;
+    }
+}
